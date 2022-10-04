4202fe33cb924438a7d27635707a69d17c250df3
Ruslan_Sharapov <Enzyme@bk.ru>
2022-10-04 14:58:50 +0300

    ToLower()

---
+++ Example05/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите ваше имя :");
+string username = Console.ReadLine();
+if(username.ToLower() == "ruslan")
+{
+    Console.WriteLine("Welcome back Ruslan!");
+}
+else
+{
+    Console.Write("Hello user ");
+    Console.WriteLine(username);
+}
