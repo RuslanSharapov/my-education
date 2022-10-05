eb758336b5f51b79134c76621dc74ba358b9719e
Ruslan_Sharapov <Enzyme@bk.ru>
2022-10-05 22:14:06 +0300

    Первое задание нахождение квадрата

---
+++ Example08/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите A ");
+int A = int.Parse(Console.ReadLine());
+Console.WriteLine("Ввекдите число B ");
+int B = int.Parse(Console.ReadLine());
+if (A * A == B)
+{       
+    Console.WriteLine("Число A является квадратом числа B");
+}
+else
+{
+    Console.WriteLine("Число A не является квадратом числа B");
+}
