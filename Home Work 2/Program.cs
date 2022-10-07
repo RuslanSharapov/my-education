1e6ee659f2c1e936024fda14107e5796f15aa9c6
Ruslan_Sharapov <Enzyme@bk.ru>
2022-10-07 10:04:36 +0300

    Убрал else

---
+++ Home Work 2/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Select day of the week 1 - 7 :");
+int week = int.Parse(Console.ReadLine());
+if (week == 1)
+{
+    Console.WriteLine("Monday");
+}
+if (week == 2)
+{
+    Console.WriteLine("Tuesday");
+}
+if (week == 3)
+{
+    Console.WriteLine("Wednesday");
+}
+if (week == 4)
+{
+    Console.WriteLine("Thursday");
+}
+if (week == 5)
+{
+    Console.WriteLine("Friday"); // УРА !!!  БУХАЕМ !!!
+}
+if (week == 6)
+{
+    Console.WriteLine("Saturday");
+}
+if (week == 7)
+{
+    Console.WriteLine("Sunday");
+}
+
+ 
