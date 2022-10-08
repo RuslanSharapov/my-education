cdb160273cac97e428bc84ade73128b9d2819dbd
Ruslan_Sharapov <Enzyme@bk.ru>
2022-10-08 06:46:32 +0300

    Найти максимальное из трех чисел

--- Home Work 1/Program.cs
+++ Home Work 1/Program.cs
@@ -1,35 +1,12 @@
-﻿Console.WriteLine("Select day of the week 1 - 7 :");
-int week = int.Parse(Console.ReadLine());
-if (week == 1)
-{
-    Console.WriteLine("Monday");
-}
-if (week == 2)
-{
-    Console.WriteLine("Tuesday");
-}
-if (week == 3)
-{
-    Console.WriteLine("Wednesday");
-}
-if (week == 4)
-{
-    Console.WriteLine("Thursday");
-}
-if (week == 5)
-{
-    Console.WriteLine("Friday"); // УРА !!!  БУХАЕМ !!!
-}
-if (week == 6)
-{
-    Console.WriteLine("Saturday");
-}
-if (week == 7)
-{
-    Console.WriteLine("Sunday");
+﻿Console.WriteLine("Введите A ");
+int A = int.Parse(Console.ReadLine());
+Console.WriteLine("Ввекдите число B ");
+int B = int.Parse(Console.ReadLine());
+if (A * A == B)
+{       
+    Console.WriteLine("Число A является квадратом числа B");
 }
 else
 {
-    Console.WriteLine("There is no such day of the week");
+    Console.WriteLine("Число A не является квадратом числа B");
 }
- 
--- Home Work 3/Program.cs
+++ Home Work 3/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число a ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число b ");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число c ");
+int c = int.Parse(Console.ReadLine());
+if(a > b && a > c)
+{
+    Console.WriteLine(a);
+}
+if(b > a && b > c)
+{
+    Console.WriteLine(b);
+}
+if(c > a && c > b)
+{
+    Console.WriteLine(c);
+}
