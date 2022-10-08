296e9b4ab837603cf6d2668ef40a884889f49a41
Ruslan_Sharapov <Enzyme@bk.ru>
2022-10-08 09:45:02 +0300

    Дано число из отрезка [10, 99]. Показать наибольшую цифру числа

---
+++ Home Work 9/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] num = { 1, 3, 6, 17, 14, 58, 99 };
+int Max = int.MinValue;
+for (int i = 0; i < num.Length; i++)
+{
+    if (Max < num[i])
+    {
+        Max = num[i];
+    }
+}
+Console.WriteLine(Max);
+Console.ReadKey();
