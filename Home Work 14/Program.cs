6eca5234be18aff586fce6749b109ab22017efe7
Ruslan_Sharapov <Enzyme@bk.ru>
2022-10-08 22:36:34 +0300

    Повторение,проверять является ли одно квадратом другого

---
+++ Home Work 14/Program.cs
@@ -0,0 +1,13 @@
+﻿// Странное задание на повторение
+Console.WriteLine("Введите A ");
+int A = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число B ");
+int B = int.Parse(Console.ReadLine());
+if (A * A == B)
+{       
+    Console.WriteLine("Число A является квадратом числа B");
+}
+else
+{
+    Console.WriteLine("Число A не является квадратом числа B");
+}
