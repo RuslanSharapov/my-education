83f18d009ab9fe0962698df6e37debc214a62f99
Ruslan_Sharapov <Enzyme@bk.ru>
2022-10-08 22:52:29 +0300

    Задать номер четверти, показать диапазоны для возможных координат

---
+++ Home Work 15/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите X: ");
+int X = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите Y: ");
+int Y = int.Parse(Console.ReadLine());
+if(X > 0 && Y > 0)
+{
+    Console.WriteLine("Первая Четверть");
+}
+if(X < 0 && Y > 0)
+{
+    Console.WriteLine("Вторая Четверть");
+}
+if(X < 0 && Y < 0)
+{
+    Console.WriteLine("Третья Четверть");
+}
+if(X > 0 && Y < 0)
+{
+    Console.WriteLine("Четвёртая Четверть");
+}
