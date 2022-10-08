ef5520164f43a776bf5ada245d4f12d903003bb2
Ruslan_Sharapov <Enzyme@bk.ru>
2022-10-08 12:03:29 +0300

    Найти третью цифру числа или сообщить, что её нет

---
+++ Home Work 12/Program.cs
@@ -0,0 +1,3 @@
+﻿int n = int.Parse(Console.ReadLine());
+int k = (int)Math.Log10(n)-2;
+Console.WriteLine(k < 0 ? "Нет третьего числа" : (n % (int)Math.Pow(10, k + 1) / (int)Math.Pow(10, k)).ToString());
