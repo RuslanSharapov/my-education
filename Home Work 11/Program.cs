e97947132965070f0b186999198a641944948e4f
Ruslan_Sharapov <Enzyme@bk.ru>
2022-10-08 11:18:10 +0300

    Выяснить, кратно ли число заданному, если нет, вывести остаток

---
+++ Home Work 11/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите первое число ");
+int A = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int B = int.Parse(Console.ReadLine());
+int result = A % B;
+if (result == 0)
+{
+    Console.WriteLine("Число A кратно числу B");
+}
+else
+{
+    Console.WriteLine("Число не кратно, остаток : " + result);
+}
