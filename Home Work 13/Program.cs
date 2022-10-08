414e55ae664b69197f76f9a69b1fb504691fc84c
Ruslan_Sharapov <Enzyme@bk.ru>
2022-10-08 22:01:37 +0300

    Дано число обозначающее день недели. Выяснить является номер дня недели выходным

---
+++ Home Work 13/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Select day of the week 1 - 7 :");
+int week = int.Parse(Console.ReadLine());
+if (week > 5 && week < 8)
+{
+    Console.WriteLine("Day off");
+}
+else
+{
+    Console.WriteLine("Working day");
+}
