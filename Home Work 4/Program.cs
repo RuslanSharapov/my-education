9df2747252ad65c5c61c040c907d961e80305051
Ruslan_Sharapov <Enzyme@bk.ru>
2022-10-08 07:09:19 +0300

    Выяснить является ли число чётным

---
+++ Home Work 4/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число ");
+int a = int.Parse(Console.ReadLine());
+if(a % 2 == 0)
+{
+    Console.WriteLine("Число а четное ");
+}
+else
+{
+    Console.WriteLine("Число не четное ");
+}
