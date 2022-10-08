52fd4915d27ad63c46c039004486a9120e338174
Ruslan_Sharapov <Enzyme@bk.ru>
2022-10-08 10:56:51 +0300

    Удалить вторую цифру трёхзначного числа

---
+++ Home Work 10/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Введите трехзначное число: ");
+int Number = int.Parse(Console.ReadLine());
+int Number2 = Number / 100 * 10 + Number % 10;
+Console.WriteLine("Новое число: " + Number2);
