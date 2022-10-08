d5b6b0afc3f6ef6d3fa38619ad61756a47d7a9a6
Ruslan_Sharapov <Enzyme@bk.ru>
2022-10-08 08:18:36 +0300

    Показать четные числа от 1 до N

---
+++ Home Work 6/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число ");
+int a = int.Parse(Console.ReadLine());
+while(a <= 100)
+{
+    int b = a % 2;
+    if(b != 1)
+    {
+        Console.WriteLine("четное " + a);
+        a++;
+    }
+    else
+    {
+        a++;
+    }
+}
