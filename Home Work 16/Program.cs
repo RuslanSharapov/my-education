a4dd4fd39474edd7eced1fb91d94c203b92dca85
Ruslan_Sharapov <Enzyme@bk.ru>
2022-10-09 09:28:27 +0300

    Программа проверяет пятизначное число на палиндромом.

---
+++ Home Work 16/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите число: ");
+string number = Console.ReadLine();
+int len = number.Length;
+if (len == 5)
+{
+    if (number[0] == number[4] && number[1] == number[3])
+    {
+        Console.WriteLine("Палиндром");
+    }
+    else
+    {
+        Console.WriteLine("Не Палиндром");
+    }
+}
+else
+{
+    Console.WriteLine("Число не пятизначное");
+}
